7ef41eff7885fe5545050eae5d48d45518e22e41
Raghu Betina <rbetina@gmail.com>
2013-05-10 11:29:28 -0500

    Wrote authentication logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by_email(params[:email])
+
+    if u.present? && u.authenticate(params[:password])
+      session[:user_id] = u.id
+      redirect_to users_url, notice: 'Sign in successful.'
+    else
+      redirect_to new_session_url, notice: 'Nice try.'
+    end
   end
 
   def destroy
